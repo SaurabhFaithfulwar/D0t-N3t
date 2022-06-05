8e9f7869feaeaa8a36f2dbbb690f49d516357567
SaurabhFaithfulwar <106900813+SaurabhFaithfulwar@users.noreply.github.com>
2022-06-05 11:39:00 +0530

    Prac1
    Basic about .Net/ First Program

---
+++ Prac1/Prac1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a, b;
+            //Console.WriteLine("Enter value of A");
+
+
+            //a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter value of B");
+
+            //b = int.Parse(Console.ReadLine());
+
+            //int c = a + b;
+
+            //Console.WriteLine("Add "+c);
+
+            //Console.ReadKey();
+
+
+
+            int[] num = { 70, 87, 97, 67};
+
+            Console.WriteLine(num.Max());
+            Console.WriteLine(num.Min());
+            Console.WriteLine(num.Sum());
+            Console.WriteLine(num.Length);
+           
+            Console.ReadKey();
+
+        }
+    }
+}
